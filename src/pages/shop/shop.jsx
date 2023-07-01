f849014a2af3cfc71cccc9e19657d1768333f574
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-01 06:27:58 +0530

    added product details on shop page

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+    <div className='shopTitle'>
+      <h1>Plumber's-Cart</h1>  
+    </div>
+    <div  className='products'>
+        
+    </div>
+    </div>
+  )
+}
+
+
---
+++ src/products.js
@@ -0,0 +1,61 @@
+import product1 from "./assets/Solar-Submersible-pump.png"
+import product2 from "./assets/monoblock.png"
+import product3 from "./assets/pressur-pump.png"
+import product4 from "./assets/water-pump-impeller.jpg"
+import product5 from "./assets/defuser-pump.jpg"
+import product6 from "./assets/submersible-pump-bush.jpg"
+import product7 from "./assets/Water-seal.png"
+import product8 from "./assets/Oil-Seal.png"
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Sumbersible Pumps",
+        price: 6000,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "Monoblock Pump",
+        price: 5000,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "pressure-pump",
+        price: 7000,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "Impeller",
+        price: 700,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "deffuser",
+        price: 200,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "bush",
+        price: 400,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Water-seal",
+        price: 70,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Oil-seal",
+        price: 40,
+        productImage: product8
+    },
+
+
+]
